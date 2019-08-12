99b05d8a5c689f3f6f50c0e2f3d3b57d378fc750
zTgx <747674262@qq.com>
2019-08-12 18:00:44 +0800

    get_profile

--- examples/main.rs
+++ examples/main.rs
@@ -1,28 +1,33 @@
-extern crate curl;
+// extern crate curl;
 
-use curl::easy::{Easy, List};
-use serde_json::{Value};
+// use curl::easy::{Easy, List};
+// use serde_json::{Value};
 
+extern crate gitapi_rs as Api;
+use Api::GitApi;
 fn main() {
-    let mut handle = Easy::new();
 
-    let mut list = List::new();
-    list.append("User-Agent: GITAPI-RS").unwrap();
-    handle.http_headers(list).unwrap();
+    println!("name: {}", GitApi::new().get_profile("zTgx")["login"]);
 
-    handle.url("https://api.github.com/users/zTgx").unwrap();
-
-    let mut buf = Vec::new();
-    {
-        let mut transfer = handle.transfer();
-        transfer.write_function(|data| {
-            buf.extend_from_slice(data);
-            Ok(data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
-    }
-
-    // println!("{:#?}", String::from_utf8(buf));
-    let info: Value = serde_json::from_slice(&buf).unwrap();
-    println!("{:#?}", info["login"]);
+    // let mut handle = Easy::new();
+    //
+    // let mut list = List::new();
+    // list.append("User-Agent: GITAPI-RS").unwrap();
+    // handle.http_headers(list).unwrap();
+    //
+    // handle.url("https://api.github.com/users/zTgx").unwrap();
+    //
+    // let mut buf = Vec::new();
+    // {
+    //     let mut transfer = handle.transfer();
+    //     transfer.write_function(|data| {
+    //         buf.extend_from_slice(data);
+    //         Ok(data.len())
+    //     }).unwrap();
+    //     transfer.perform().unwrap();
+    // }
+    //
+    // // println!("{:#?}", String::from_utf8(buf));
+    // let info: Value = serde_json::from_slice(&buf).unwrap();
+    // println!("{:#?}", info["login"]);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,44 @@
 
+use serde_json::{Value};
+extern crate curl;
+use curl::easy::{Easy, List};
+
 static HOST: &'static str = "https://api.github.com";
+
+pub struct GitApi {
+    pub engine: Easy,
+}
+impl GitApi {
+    pub fn new() -> Self {
+        let mut handle = Easy::new();
+        GitApi {
+            engine: handle,
+        }
+    }
+}
+
+impl GitApi {
+    pub fn get_profile(&mut self, name: &str) -> Value {
+        let mut list = List::new();
+        list.append("User-Agent: GITAPI-RS").unwrap();
+        self.engine.http_headers(list).unwrap();
+
+        self.engine.url("https://api.github.com/users/zTgx").unwrap();
+
+        let mut buf = Vec::new();
+        {
+            let mut transfer = self.engine.transfer();
+            transfer.write_function(|data| {
+                buf.extend_from_slice(data);
+                Ok(data.len())
+            }).unwrap();
+            transfer.perform().unwrap();
+        }
+
+        // println!("{:#?}", String::from_utf8(buf));
+        let info: Value = serde_json::from_slice(&buf).unwrap();
+        // println!("{:#?}", info["login"]);
+
+        info
+    }
+}
