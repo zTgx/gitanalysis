858f1fd4509cfd9ebec12c22b886ae10c79979df
zTgx <747674262@qq.com>
2019-08-12 17:47:11 +0800

    add path /users

--- examples/main.rs
+++ examples/main.rs
@@ -1,7 +1,28 @@
-extern crate gitapi_rs;
-use gitapi_rs::Git;
+extern crate curl;
+
+use curl::easy::{Easy, List};
+use serde_json::{Value};
 
 fn main() {
-    let all: String = Git::all_branch();
-    println!("all branch: {}", all);
+    let mut handle = Easy::new();
+
+    let mut list = List::new();
+    list.append("User-Agent: GITAPI-RS").unwrap();
+    handle.http_headers(list).unwrap();
+
+    handle.url("https://api.github.com/users/zTgx").unwrap();
+
+    let mut buf = Vec::new();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|data| {
+            buf.extend_from_slice(data);
+            Ok(data.len())
+        }).unwrap();
+        transfer.perform().unwrap();
+    }
+
+    // println!("{:#?}", String::from_utf8(buf));
+    let info: Value = serde_json::from_slice(&buf).unwrap();
+    println!("{:#?}", info["login"]);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,2 @@
-use std::process::Command;
 
-pub struct Git {}
-impl Git {
-    pub fn all_branch() -> String {
-        let output = Command::new("git")
-                         .arg("branch")
-                         .arg("-a")
-                         .output()
-                         .expect("Failed to execute command");
-         let output = output.stdout.as_slice().to_vec();
-
-         String::from_utf8(output).unwrap()
-    }
-}
+static HOST: &'static str = "https://api.github.com";
