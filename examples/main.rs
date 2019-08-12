d89503214552e84beb7a2bf01c9349c264b4dde9
zTgx <747674262@qq.com>
2019-08-12 19:11:42 +0800

    add struct Path

--- examples/main.rs
+++ examples/main.rs
@@ -1,33 +1,5 @@
-// extern crate curl;
-
-// use curl::easy::{Easy, List};
-// use serde_json::{Value};
-
 extern crate gitapi_rs as Api;
 use Api::GitApi;
 fn main() {
-
-    println!("name: {}", GitApi::new().get_profile("zTgx")["login"]);
-
-    // let mut handle = Easy::new();
-    //
-    // let mut list = List::new();
-    // list.append("User-Agent: GITAPI-RS").unwrap();
-    // handle.http_headers(list).unwrap();
-    //
-    // handle.url("https://api.github.com/users/zTgx").unwrap();
-    //
-    // let mut buf = Vec::new();
-    // {
-    //     let mut transfer = handle.transfer();
-    //     transfer.write_function(|data| {
-    //         buf.extend_from_slice(data);
-    //         Ok(data.len())
-    //     }).unwrap();
-    //     transfer.perform().unwrap();
-    // }
-    //
-    // // println!("{:#?}", String::from_utf8(buf));
-    // let info: Value = serde_json::from_slice(&buf).unwrap();
-    // println!("{:#?}", info["login"]);
+    println!("name: {}", GitApi::new().get_profile("/users/zTgx")["login"]);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,50 @@ use curl::easy::{Easy, List};
 
 static HOST: &'static str = "https://api.github.com";
 
+pub struct Header {}
+impl Header {
+    pub fn get() -> List {
+        let mut list = List::new();
+        list.append("User-Agent: GITAPI-RS").unwrap();
+
+        list
+    }
+}
+
+pub struct Config {}
+impl Config {
+    pub fn host() -> &'static str {
+        HOST
+    }
+
+    pub fn get_url(path: &'static str) -> String {
+        Config::host().to_owned() + path
+    }
+}
+
 pub struct GitApi {
+    pub host: &'static str,
     pub engine: Easy,
 }
 impl GitApi {
     pub fn new() -> Self {
-        let mut handle = Easy::new();
+        let handle = Easy::new();
         GitApi {
+            host: Config::host(),
             engine: handle,
         }
+    }
+
+    pub fn headers(&mut self) {
+        self.engine.http_headers(Header::get()).unwrap();
     }
 }
 
 impl GitApi {
-    pub fn get_profile(&mut self, name: &str) -> Value {
-        let mut list = List::new();
-        list.append("User-Agent: GITAPI-RS").unwrap();
-        self.engine.http_headers(list).unwrap();
+    pub fn get_profile(&mut self, path: &'static str) -> Value {
+        self.headers();
 
-        self.engine.url("https://api.github.com/users/zTgx").unwrap();
+        self.engine.url(&Config::get_url(path)).unwrap();
 
         let mut buf = Vec::new();
         {
